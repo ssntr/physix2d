6d25522a261eee7a8a3920a5c036c9981114216a
ssntr <142799815+ssntr@users.noreply.github.com>
2025-02-24 15:36:56 +0200

    draw_from_config added to config

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+labels = ["A", "B", "C"]
+
+def draw_from_config(x_list,y_list):
+    plt.figure(figsize=(5,5))
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+    plt.plot(x_list, y_list)
+
+    for i, label in enumerate(labels):
+        if i < len(labels):
+            plt.text(x_list[i], y_list[i], label, fontsize=8, ha='right', va='bottom', color='red')
+    plt.show()
